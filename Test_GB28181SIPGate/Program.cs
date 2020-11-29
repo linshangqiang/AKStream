db07f6ff15d562f9fb0089f4d5e7b135a2c0826d
qiuzhouwei <chatop@qq.com>
2020-11-29 21:57:07 +0800

    first commit

---
+++ Test_GB28181SIPGate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_GB28181SIPGate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
